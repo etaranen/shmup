d62a1b6dfee4de5d9babd6750c10e9ae5e2295bc
etaranen <elizabeth.taranen@gmail.com>
2026-01-21 17:52:28 -0500

    added enemies and player shooting

--- shmup_game/Assets/Scripts/PlayerScript.cs
+++ shmup_game/Assets/Scripts/PlayerScript.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /// <summary>
+    /// 1 - The speed of the ship
+    /// </summary>
+    public Vector2 speed = new Vector2(50, 50);
 
-    // Update is called once per frame
+    // 2 - Store the movement and the component
+    private Vector2 movement;
+    private Rigidbody2D rigidbodyComponent;
+
     void Update()
     {
-        
+        // 3 - Retrieve axis information
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        // 4 - Movement per direction
+        movement = new Vector2(
+            speed.x * inputX,
+            speed.y * inputY);
+
+        // 5 - Shooting
+        bool shoot = Input.GetButtonDown("Fire1");
+        shoot |= Input.GetButtonDown("Fire2");
+        // Careful: For Mac users, ctrl + arrow is a bad idea
+
+        if (shoot)
+            {
+                WeaponScript weapon = GetComponent<WeaponScript>();
+                if (weapon != null)
+            {
+                // false because the player is not an enemy
+                weapon.Attack(false);
+            }
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        // 5 - Get the component and store the reference
+        if (rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody2D>();
+
+        // 6 - Move the game object
+        rigidbodyComponent.velocity = movement;
     }
 }
